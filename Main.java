5669e42db9979cab26dc37c03c7efda77ebd9d8f
FourtTzxc <sasha2009chernov@gmail.com>
2024-08-26 11:16:59 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,1 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        Window window = new Window();
-        window.open();
 
-
-    }
-}
